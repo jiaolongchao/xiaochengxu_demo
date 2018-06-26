cd70e4c1f62c11e7135fc95115153dcb6c0bc5f9
jiaolongchao <jiaolongchao@9086.cn>
2018-06-26 11:06:00 +0800

    测评首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,42 +4,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    contentItems:['','','',''],
+    listItem:['','','','','']
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log('---indexPage onLoad---')
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    console.log('---indexPage onReady---')
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    console.log('---indexPage onShow---')
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    console.log('---indexPage onHide---')
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    console.log('---indexPage onUnload---')
+    
   },
 
   /**
@@ -61,36 +67,5 @@ Page({
    */
   onShareAppMessage: function () {
     
-  },
-  itemClick:function(){
-    wx.navigateTo({
-      url: '../logs/logs?id=1',
-    })
-  },
-
-  
-  canvasIdErrorCallback: function (e) {
-    console.error(e.detail.errMsg)
-  },
-  onReady: function (e) {
-    // 使用 wx.createContext 获取绘图上下文 context
-    var context = wx.createCanvasContext('firstCanvas')
-
-    context.setStrokeStyle("#00ff00")
-    context.setLineWidth(5)
-    context.rect(0, 0, 200, 200)
-    context.stroke()
-    context.setStrokeStyle("#ff0000")
-    context.setLineWidth(2)
-    context.moveTo(160, 100)
-    context.arc(100, 100, 60, 0, 2 * Math.PI, true)
-    context.moveTo(140, 100)
-    context.arc(100, 100, 40, 0, Math.PI, false)
-    context.moveTo(85, 80)
-    context.arc(80, 80, 5, 0, 2 * Math.PI, true)
-    context.moveTo(125, 80)
-    context.arc(120, 80, 5, 0, 2 * Math.PI, true)
-    context.stroke()
-    context.draw()
   }
 })
